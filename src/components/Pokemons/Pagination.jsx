37d95318bb565cf1f3d04f0aa194ad4235aa9885
German B <geuxor@dr.com>
2021-10-23 18:44:09 +0200

    feat-pagination

---
+++ src/components/Pokemons/Pagination.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import "./PokemonPage.style.css";
+export default function Pagination({
+  gotoNextPage,
+  gotoPrevPage,
+  onItemsPerPageChange,
+  pokemonsPerPage,
+  onPrevNextPageChange,
+  isLoading,
+}) {
+
+  return (
+    <div>
+      {console.log("Pagination: pokemonsPerPage", pokemonsPerPage)}
+      {console.log("Pagination: next/prev", gotoPrevPage, gotoNextPage)}
+
+      <div className="pagination-buttons">
+        {gotoPrevPage && (
+          <button onClick={() => onPrevNextPageChange(gotoPrevPage)}>
+            Previous
+          </button>
+        )}
+        {gotoNextPage && (
+          <button onClick={() => onPrevNextPageChange(gotoNextPage)}>
+            Next
+          </button>
+        )}
+        {onItemsPerPageChange && (
+          <select
+            className="pagination-select"
+            value={pokemonsPerPage}
+            onChange={(e) => {
+              onItemsPerPageChange(Number(e.target.value));
+            }}
+          >
+            {[10, 20, 50].map((itemsPerPage) => (
+              <>
+                {console.log("Pagination pokemonsPerPage", pokemonsPerPage)}
+                <option key={itemsPerPage} value={itemsPerPage}>
+                  Show {itemsPerPage}
+                </option>
+              </>
+            ))}
+          </select>
+        )}
+      </div>
+    </div>
+  );
+}
